8de37612f7d330009cbdfe7ca13fbd886d1b42c6
aunn <naris@flyingcomma.com>
2014-02-13 12:12:14 +0700

    add insert test

--- AccessLogAPI.php
+++ AccessLogAPI.php
@@ -12,42 +12,29 @@ class AccessLogAPI {
 		return $stmt->rowCount();
 	}
 	
-	function insert($value_array)
-	{
+	function insert($value_array) {
 		$sql = "INSERT INTO api_log(ip, service_name, `datetime`, response_time,response_massage) VALUES (:ip,:service_name,:datetime,:response_time,:response_massage)";
 		$q = $this->db->prepare($sql);
 
 		return $q->execute(
 			[
-				$value_array['ip']=>$ip,
-				$value_array['service_name']=>$service_name,
-				$value_array['datetime']=>$datetime,
-				$value_array['response_time']=>$response_time,
-				$value_array['response_massage']=>$response_massage
+				':ip'=>$value_array['ip'],
+				':service_name'=>$value_array['service_name'],
+				':datetime'=>$value_array['datetime'],
+				':response_time'=>$value_array['response_time'],
+				':response_massage'=>$value_array['response_massage']
 			]
 	    );
-
 	}
 	
-	function updateById($id, $sql){
-		//	try {
-			//$dbh = new PDO('mysql:host=localhost;dbname='.$database, $user, $pass);
-			$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		//try {
-			$stmt = $this->db->prepare("UPDATE ".$table." SET ip=:ip where id=1");
-			//$now = date("Y-m-d H:m:i");
-			//$stmt->bindParam(':now', $now);
-			$ip = '158.168.2.7';
-					$stmt->bindParam(':ip', $ip);
-	//$stmt->bindParam(':value', $value);
-			$dbh->beginTransaction();
-			//$dbh->exec("insert into ".$table." (ip, service_name, datetime, response_time,response_message) values('192.168.1.1', 'mysqld', '".date("Y-m-d H:m:s")."', '".date("Y-m-d H:m:s")."', 'OK')");
-		//$dbh->exec("insert into salarychange (id, amount, changedate) values (23, 50000, NOW())");
-			$stmt->execute();
-			$dbh->commit();
-		//} catch (Exception $e){
-		//	echo $e->getMessage();
-		//}	
+	function updateById($id, $sql) {
+		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$stmt = $this->db->prepare("UPDATE ".$table." SET ip=:ip where id=1");		
+		$ip = '158.168.2.7';
+		$stmt->bindParam(':ip', $ip);
+		$dbh->beginTransaction();	
+		$stmt->execute();
+		$dbh->commit();
 	}
 }
 ?>
--- AccessLogAPITest.php
+++ AccessLogAPITest.php
@@ -1,6 +1,49 @@
 <?php
 	require_once 'AccessLogAPI.php';
-	class AccessLogAPITest extends PHPUnit_Framework_Testcase {
+	class AccessLogAPITest extends PHPUnit_Framework_TestCase {
+		function setUp() {
+			$this->mockPDOStatement = $this->getMock('PDOStatement');
+			$this->mockPDO = $this->getMock('MockPDO');
+			$this->object = new AccessLogAPI();
+		}
+		function testInsert() {
+			$object = new AccessLogAPI();
+			$dbh = new PDO('mysql:host=localhost;dbname=access_log', 'root', '1234');
+			$object->setPDO($dbh);
+			$result = $object->insert(null);
+			$this->assertTrue($result);
+		}
+
+		function testInsertMockSuccess() {
+			$this->mockPDOStatement->expects($this->exactly(1))
+             ->method('execute')
+             ->will($this->returnValue(true));
+
+			$this->mockPDO->expects($this->exactly(1))
+             ->method('prepare')
+             ->will($this->returnValue($this->mockPDOStatement));
+
+			$this->object->setPDO($this->mockPDO);
+			$result = $this->object->insert(null);
+			$this->assertTrue($result);
+		}
+		function testInsertMockFail() {
+			$this->mockPDOStatement->expects($this->exactly(1))
+             ->method('execute')
+             ->will($this->returnValue(false));
+            
+			$this->mockPDO->expects($this->exactly(1))
+             ->method('prepare')
+             ->will($this->returnValue($this->mockPDOStatement));
+
+			$this->object->setPDO($this->mockPDO);
+			$result = $this->object->insert(null);
+			$this->assertFalse($result);
+		}
 		
 	}
+
+class MockPDO extends PDO {
+	function __construct() {}
+}
 ?>
